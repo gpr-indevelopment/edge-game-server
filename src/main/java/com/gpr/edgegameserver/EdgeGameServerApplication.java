4c386ed29f1348078d2625e7ea850cfcba716c43
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2021-09-04 16:44:45 -0300

    Initializer

---
+++ src/main/java/com/gpr/edgegameserver/EdgeGameServerApplication.java
@@ -0,0 +1,13 @@
+package com.gpr.edgegameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeGameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeGameServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpr/edgegameserver/EdgeGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpr.edgegameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeGameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
